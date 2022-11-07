11cc169eed5dd571acf8256bd46d1ed209cb97f5
พงศ์ภรณ์ ถนอมในเมือง <pongporn.t@kkumail.com>
2022-11-07 22:47:29 +0700

    Initial commit

---
+++ storage/framework/views/ca4e09240c1b58539db14d60073d770c5f4d7d62.php
@@ -0,0 +1,93 @@
+<footer class="bg-pink-300 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2017-2021 Code With Dary. All Rights Reserved Edited By 633410082-7
+    </p>
+</footer><?php /**PATH C:\Users\Pong Tanomnaimuang\Desktop\BLOG\633410082-7-Laravel-Blog-Project\633410082-7 Laravel Blog Project\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
